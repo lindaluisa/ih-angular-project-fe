c36c2c4d1cd4eb4654f694e197e8b368fa508cdd
Victor Pellerin <pellerin.victor@gmail.com>
2018-03-03 18:59:23 +0100

    create list stories and single story 2

---
+++ src/app/components/single-story/single-story.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-story',
+  templateUrl: './single-story.component.html',
+  styleUrls: ['./single-story.component.css']
+})
+export class SingleStoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
